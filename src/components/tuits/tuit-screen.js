e9c11df8df94cff185f9d73002404d9fe7bb2f02
Frankie Luo <luo.jingy@northeastern.edu>
2022-04-29 01:08:36 -0400

    js doc.

--- src/components/tuits/tuit-screen.js
+++ src/components/tuits/tuit-screen.js
@@ -1,19 +1,21 @@
-import {useEffect, useState} from "react";
-import * as service from "../../services/tuits-service"
-import {useParams} from "react-router-dom";
-import Tuit from "./tuit";
+import { useEffect, useState } from 'react'
+import * as service from '../../services/tuits-service'
+import { useParams } from 'react-router-dom'
+import Tuit from './tuit'
 
+/**
+ * @component renders the tuits screen
+ */
 const TuitScreen = () => {
-    const [tuit, setTuit] = useState({});
-    const {tid} = useParams();
-    const findTuitById = () =>
-        service.findTuitById(tid)
-            .then(tuit => setTuit(tuit));
-    useEffect(findTuitById, []);
-    return(
-        <div>
-            <Tuit tuit={tuit} likeTuit={() => {}}/>
-        </div>
-    );
-};
-export default TuitScreen;
+  const [tuit, setTuit] = useState({})
+  const { tid } = useParams()
+  const findTuitById = () =>
+    service.findTuitById(tid).then((tuit) => setTuit(tuit))
+  useEffect(findTuitById, [tid])
+  return (
+    <div>
+      <Tuit tuit={tuit} likeTuit={() => {}} />
+    </div>
+  )
+}
+export default TuitScreen
--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -1,88 +1,92 @@
-import React from "react";
-import { useEffect, useState } from "react";
-import * as service from "../../services/tuits-service";
-import * as tuitService from "../../services/tuits-service";
+import React from 'react'
+import { useEffect, useState } from 'react'
+import * as service from '../../services/tuits-service'
+import * as tuitService from '../../services/tuits-service'
 
-
-const TuitStats = ({ tuit, likeTuit = () => {}, dislikeTuit = () => {}, profile }) => {
-  const [ifLike, setIfLike] = useState(null);
-//privateStatus
-  const [privateStatus, setPrivateStatus] = useState("Set Private");
-
+const TuitStats = ({
+  tuit,
+  likeTuit = () => {},
+  dislikeTuit = () => {},
+  profile,
+}) => {
+  const [ifLike, setIfLike] = useState(null)
+  const [privateStatus, setPrivateStatus] = useState('Set Private')
 
   const changePrivate = async () => {
     await tuitService.updateTuit(tuit._id, {
       ...tuit,
-      isPrivate: !tuit.isPrivate
-    });
+      isPrivate: !tuit.isPrivate,
+    })
 
     setPrivateStatus(
-      privateStatus == "Set Private" ? "Set Public" : "Set Private"
-    );
-  };
-
+      privateStatus == 'Set Private' ? 'Set Public' : 'Set Private'
+    )
+  }
 
   const findIfLike = () =>
-    service.findIfUserLikesTuit("me", tuit._id).then(like => setIfLike(like));
-  //useEffect(findIfLike, []);
+    service.findIfUserLikesTuit('me', tuit._id).then((like) => setIfLike(like))
 
-  const [ifDislike, setIfDislike] = useState(null);
+  const [ifDislike, setIfDislike] = useState(null)
   const findIfDislike = () =>
     service
-      .findIfUserDislikesTuit("me", tuit._id)
-      .then(dislike => setIfDislike(dislike));
+      .findIfUserDislikesTuit('me', tuit._id)
+      .then((dislike) => setIfDislike(dislike))
 
   const clickLike = async () => {
-    await likeTuit(tuit);
-    await findIfLike();
-    //await findIfDislike();
-  };
+    await likeTuit(tuit)
+    await findIfLike()
+  }
 
   const clickDislike = async () => {
-    await dislikeTuit(tuit);
-    await findIfDislike();
-    //await findIfLike();
-  };
+    await dislikeTuit(tuit)
+    await findIfDislike()
+  }
 
   return (
-    <div className="row mt-2">
-      <div className="col">
-        <i className="far fa-message me-1"></i>
+    <div className='row mt-2'>
+      <div className='col'>
+        <i className='far fa-message me-1'></i>
         {tuit.stats && tuit.stats.replies}
       </div>
-      <div className="col">
-        <i className="far fa-retweet me-1"></i>
+      <div className='col'>
+        <i className='far fa-retweet me-1'></i>
         {tuit.stats && tuit.stats.retuits}
       </div>
-      <div className="col">
+      <div className='col'>
         <span onClick={() => clickLike()}>
           {ifLike ? (
-            <i className="fas fa-thumbs-up me-1" style={{ color: "red" }}></i>
+            <i className='fas fa-thumbs-up me-1' style={{ color: 'red' }}></i>
           ) : (
-            <i className="far fa-thumbs-up me-1"></i>
+            <i className='far fa-thumbs-up me-1'></i>
           )}
           {tuit.stats && tuit.stats.likes}
         </span>
       </div>
-      <div className="col">
+      <div className='col'>
         <span onClick={() => clickDislike()}>
           {ifDislike ? (
-            <i className="fas fa-thumbs-down me-1" style={{ color: "red" }}></i>
+            <i className='fas fa-thumbs-down me-1' style={{ color: 'red' }}></i>
           ) : (
-            <i className="far fa-thumbs-down me-1"></i>
+            <i className='far fa-thumbs-down me-1'></i>
           )}
           {tuit.stats && tuit.stats.dislikes}
         </span>
       </div>
-      <div className="col">
-        <i className="far fa-inbox-out"></i>
+      <div className='col'>
+        <i className='far fa-inbox-out'></i>
       </div>
-       {tuit.postedBy._id == profile._id && (
-              <div className="col">
-                <button type="button" className="btn btn-outline-primary btn-sm" onClick={changePrivate}>{privateStatus}</button>
-              </div>
-            )}
+      {tuit.postedBy._id == profile._id && (
+        <div className='col'>
+          <button
+            type='button'
+            className='btn btn-outline-primary btn-sm'
+            onClick={changePrivate}
+          >
+            {privateStatus}
+          </button>
+        </div>
+      )}
     </div>
-  );
-};
-export default TuitStats;
+  )
+}
+export default TuitStats
